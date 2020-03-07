1ca887010d6d1dd2d55b5f1a83664df7d8a8958f
Gerard Hynes <gkhynes@gmail.com>
2020-03-07 21:57:11 +0000

    Add useMemo example

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useRef, createContext } from "react";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  createContext,
+  useMemo
+} from "react";
 import Toggle from "./Toggle";
 import Counter from "./Counter";
 import { useTitleInput } from "./hooks/useTitleInput";
@@ -7,12 +13,24 @@ const App = () => {
   const [name, setName] = useTitleInput("");
   const ref = useRef();
 
+  const reverseWord = word => {
+    console.log("function called");
+    return word
+      .split("")
+      .reverse()
+      .join("");
+  };
+
+  const title = "React Hooks Demo";
+
+  const TitleReversed = useMemo(() => reverseWord(title), [title]);
+
   return (
     <div className="main-wrapper" ref={ref}>
-      <h1>Hooks Demo</h1>
-      <Counter />
+      <h1>{TitleReversed}</h1>
+      {/* <Counter /> */}
       {/* <Toggle /> */}
-      {/* <form
+      <form
         onSubmit={e => {
           e.preventDefault();
         }}
@@ -23,7 +41,7 @@ const App = () => {
           value={name}
         />
         <button>Submit</button>
-      </form> */}
+      </form>
     </div>
   );
 };
